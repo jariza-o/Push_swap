cc76607f8771e56e5a9c0f2507a8876101f9719e
jariza-o <juan.ariza@gmail.com>
2023-06-12 18:47:29 +0200

    errors ready

--- Code/errors.c
+++ Code/errors.c
@@ -24,12 +24,16 @@ int	check_rep(char **argv)
 	n = 0;
 	while (argv[n++]) //ver si el n++ hay esta bien
 		size++;
-	nums = (int *)malloc(sizeof(int) * (size + 1)); //tiene que sumar el +1??
+	nums = (int *)malloc(sizeof(int) * (size)); //tiene que sumar el +1??
 	if (nums == NULL)
-		return (NULL);
+		return (1); //he cambiado NULL por 1 porque tiene que devolver 1, y s1 significa que hay repetido o fallo
 	n = 0;
-	while (argv[n++]) //ver si el n++ hay esta bien
-		nums[i] = ft_atol(argv[n]);
+	while (argv[n]) //ver si el n++ hay esta bien
+	{
+		nums[i] = ft_atoi(argv[n]);
+		i++;
+		n++;
+	}
 	i = 1;
 	n = 0;
 	while (nums[n])
@@ -37,12 +41,15 @@ int	check_rep(char **argv)
 		while (nums[i])
 		{
 			if (nums[n] == nums[i] && n != i)
+			{
+				free(nums);
 				return (1);
+			}
 			i++;
 		}
 		n++;
+		i = n;
 	}
-	free(nums); //ver si hay que liberar o no
 	return (0);
 }
 
@@ -52,13 +59,13 @@ int	check_error(char **argv)
 	int			n;
 	long int	num;
 
-	i = 0;
 	n = 0;
 	while (argv[n])
 	{
+		i = 0;
 		while (argv[n][i])
 		{
-			if (ft_strcmp("", argv[n]) == 0)
+			if (ft_strncmp("", argv[n], 1) == 0) //compara
 				return (1);
 			else if ((argv[n][i] == '-' || argv[n][i] == '+') &&
 			(argv[n][i + 1] == '-' || argv[n][i + 1] == '+'))
@@ -70,7 +77,6 @@ int	check_error(char **argv)
 		num = ft_atol(argv[n]);
 		if (num < INT_MIN || num > INT_MAX) //Comprobar si el numero está entre el número mínimo y el número maximo que soporta un long int
 			return (1);
-		i = 0;
 		n++;
 	}
 	return (0);
